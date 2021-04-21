d5cee07663defcc9694b35ec83e860eb45e84970
Caseywalker <casey.walker118@gmail.com>
2021-04-21 16:19:25 -0500

    form containers

--- src/App/index.js
+++ src/App/index.js
@@ -1,36 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import getJoke from '../helpers/data/jokeData';
 import './App.scss';
 
 function App() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
+  const [singleJoke, setSingleJoke] = useState({});
+  const [showPunchline, setShowPunchline] = useState(false);
+  const [userInput, setUserInput] = useState({
+    artist: '',
+    title: ''
+  });
 
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
+  const getSingleJoke = () => {
+    getJoke().then((joke) => {
+      setSingleJoke(joke);
+    });
   };
+
+  const handleClick = () => {
+    if (showPunchline) {
+      setShowPunchline(false);
+      getSingleJoke();
+    } else {
+      setShowPunchline(true);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.warn(userInput);
+    setUserInput({
+      artist: '',
+      title: ''
+    });
+  };
+
+  const handleUserInput = (e) => {
+    setUserInput({
+      ...userInput,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  useEffect(() => {
+    getSingleJoke();
+  }, []);
 
   return (
     <div className='App'>
-      <h2>INSIDE APP COMPONENT</h2>
-      <div>
-        <button
-          id='this-button'
-          className='btn btn-info'
-          onClick={handleClick}
-        >
-          I am THIS button
+      <h1>{singleJoke.setup}</h1>
+      <h3>{showPunchline ? singleJoke.punchline : ''}</h3>
+      <button onClick={handleClick}>{showPunchline ? 'Get Another Joke' : 'Get Punchline'}</button>
+      <p>{userInput.artist}</p>
+      <p>{userInput.title}</p>
+      <form onSubmit={handleSubmit}>
+        <input id='artist' value={userInput.artist} onChange={handleUserInput}>
+        </input>
+        <input id='title' value={userInput.title} onChange={handleUserInput}>
+        </input>
+        <button type='submit'>
+          Submit
         </button>
-      </div>
-      <div>
-        <button
-          id='that-button'
-          className='btn btn-primary mt-3'
-          onClick={handleClick}
-        >
-          I am THAT button
-        </button>
-      </div>
-      <h3>{domWriting}</h3>
+      </form>
     </div>
   );
 }
